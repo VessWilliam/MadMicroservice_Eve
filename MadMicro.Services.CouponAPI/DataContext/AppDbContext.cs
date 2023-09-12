9bbc445531b0c7caf0c898764735150929b8d428
vesswilliam <vess1994@gmail.com>
2023-09-13 07:55:44 +0800

    Add project files.

---
+++ MadMicro.Services.CouponAPI/DataContext/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MadMicro.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MadMicro.Services.CouponAPI.DataContext;
+
+public class AppDbContext :DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Coupon> Coupons { get; set; }
+}
---
+++ MadMicro.Services.CouponAPI/Models/Coupon.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MadMicro.Services.CouponAPI.Models;
+
+public class Coupon
+{
+    [Key]
+    public int CouponId { get; set; }
+    [Required]
+    public string CouponCode { get; set; }
+    [Required]
+    public double DiscountAmount { get; set; }
+    [Required]
+    public int MinAmount { get; set; }
+
+}
