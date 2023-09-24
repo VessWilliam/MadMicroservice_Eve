51ac6e4d786b5c0cf0dda0698a7d1cdb2adc6f0a
vesswilliam <vess1994@gmail.com>
2023-09-24 17:13:54 +0800

    clean code added extension for swagger

--- MadMicro.Services.CouponAPI/Program.cs
+++ MadMicro.Services.CouponAPI/Program.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using MadMicro.Services.CouponAPI;
 using MadMicro.Services.CouponAPI.DataContext;
+using MadMicro.Services.CouponAPI.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -20,56 +21,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(option =>
-{
-    option.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Description = "Enter The Bearer Authorizing string as following: `Bearer Generated-JWT-Token`",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
-    option.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference= new OpenApiReference
-                {
-                    Type=ReferenceType.SecurityScheme,
-                    Id=JwtBearerDefaults.AuthenticationScheme
-                }
-            }, new string[]{}
-        }
-    });
-});
 
-var secret = builder.Configuration.GetValue<string>("AppSettings:Secret");
-var issuer = builder.Configuration.GetValue<string>("AppSettings:Issuer");
-var audience = builder.Configuration.GetValue<string>("AppSettings:Audience");
-
-var key = Encoding.ASCII.GetBytes(secret!);
-
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-
-}).AddJwtBearer(x =>
-{
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidIssuer = issuer,
-        ValidAudience = audience,
-        ValidateAudience = true,
-        ValidateIssuer = true,
-        ValidateIssuerSigningKey = true,
-    };
-
-});
-
+builder.AddAppAuthetication();
 builder.Services.AddAuthorization();
 var app = builder.Build();
 
