dd4368a96948ef87b774a282fe0d6d15dd34b588
vesswilliam <vess1994@gmail.com>
2023-11-10 06:07:25 +0800

    modify Check out UI

--- MadMicro.Web/Models/CartHeadersDTO.cs
+++ MadMicro.Web/Models/CartHeadersDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace MadMicro.Web.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MadMicro.Web.Models;
 
 public class CartHeadersDTO
 {
@@ -7,8 +9,10 @@ public class CartHeadersDTO
     public string? CouponCode { get; set; }
     public double Discount { get; set; }
     public double CartTotal { get; set; }
-    public string? FirstName { get; set; }
-    public string? LastName { get; set; }
+    [Required]
+    public string? Name { get; set; }
+    [Required]
     public string? Phone { get; set; }
+    [Required]
     public string? Email { get; set; }
 }
