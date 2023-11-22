23e75e7d6a777abecaa3643373f98c95eb8991c2
vesswilliam <vess1994@gmail.com>
2023-11-23 07:24:46 +0800

    Reward Service

---
+++ MadMicro.Services.RewardAPI/Services/IService/IRewardsService.cs
@@ -0,0 +1,9 @@
+﻿using MadMicro.Services.RewardAPI.Message;
+
+namespace MadMicro.Services.RewardAPI.Services.IService;
+
+public interface IRewardsService
+{
+    Task UpdateRewards(RewardMessage rewardMessage);
+
+}
---
+++ MadMicro.Services.RewardAPI/Services/Service/RewardsService.cs
@@ -0,0 +1,40 @@
+﻿using MadMicro.Services.RewardAPI.DataContext;
+using MadMicro.Services.RewardAPI.Message;
+using MadMicro.Services.RewardAPI.Models;
+using MadMicro.Services.RewardAPI.Services.IService;
+using Microsoft.EntityFrameworkCore;
+
+namespace MadMicro.Services.RewardAPI.Services.Service;
+
+public class RewardsService : IRewardsService
+{
+    private readonly DbContextOptions<AppDbContext> _dbContext;
+
+    public RewardsService(DbContextOptions<AppDbContext> dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task UpdateRewards(RewardMessage rewardMessage)
+    {
+        try
+        {
+            Rewards rewards = new()
+            {
+                OrderId = rewardMessage.OrderId,
+                RewardsActivity = rewardMessage.RewardsActivity,
+                UserId = rewardMessage.UserId,  
+                RewardsDate = DateTime.Now,
+            };
+            await using var _context = new AppDbContext(_dbContext);
+            await _context.Rewards.AddAsync(rewards);
+            await _context.SaveChangesAsync();  
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+}
+
